a259ce3ee30242bc4373a4cfce47899e7c75049e
Ethan Smyth <es2005@hw.ac.uk>
2024-01-19 16:33:28 +0000

    update create_post

--- create_post.php
+++ create_post.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    if (! isset($_SESSION["user_id"])){
+        header('Location: '."./register.php");
+    }
     if (isset($_POST['register'])) {
         // Connect to the database
         require_once "connect_db.php";
