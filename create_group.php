8b1f4eda83632374b8b66887f330ddd60ba6b26c
Ethan Smyth <es2005@hw.ac.uk>
2024-01-19 16:55:13 +0000

    update create_group.php

--- create_group.php
+++ create_group.php
@@ -1,3 +1,10 @@
+<?php
+if (isset($_POST['listValues'])) {
+    $inputValues = $_POST['listValues'];
+    $valuesArray = explode(', ', $inputValues);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +16,10 @@
 <form action="create_group.php" method="post">
 <label for="group_name">Group name:</label> 
   <input id="group_name" name="name" type="text"/>
-  <label for="username">Username:</label> 
-  <input id="username" name="username" required="" type="text"/>
-  <label for="email">Email:</label>
-  <input id="email" name="email" required="" type="email" />
-  <label for="password">Password:</label>
-  <input id="password" name="password" required="" type="password" />
-  <input name="register" type="submit" value="Register" />
+  <label for="listValues">Enter a list of initial group member usernames (not including yourself) separated by commas:</label>
+  <input type="text" id="listValues" name="listValues">
+  <br>
+  <input type="submit" value="Submit">
 </form>
 </body>
 </html>
