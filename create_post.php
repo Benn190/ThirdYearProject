c216c1abb3864b26c1c88dbaf802c4560063801f
Ethan Smyth <es2005@hw.ac.uk>
2024-01-19 16:19:29 +0000

    complete create_post functionality

--- create_post.php
+++ create_post.php
@@ -11,7 +11,7 @@
         $user_id = $_SESSION['user_id'];
         $text = $_POST['text'];
 
-        $result = pg_execute($conn, "insert_post", array($username, $text));
+        $result = pg_execute($conn, "insert_post", array($user_id, $text));
          if ($result) {
             echo "Post created successfully!";
             $post_id = pg_fetch_result($result, 0, 'post_id');
@@ -20,8 +20,8 @@
             echo "Error: " . pg_last_error($conn);
         }
     
-        $uploaddir = './post_images';
-        $uploadfile = $uploaddir . basename($_FILES['post_image']['image$post_id.png']);
+        $uploaddir = './post_images/';
+        $uploadfile = $uploaddir . "post_image". $post_id . ".png";
 
         if(move_uploaded_file($_FILES['post_image']['tmp_name'], $uploadfile)){
         echo "File was successfully uploaded.\n";
@@ -45,13 +45,13 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="create_post.php" method="post">
+    <form action="create_post.php" method="post" enctype="multipart/form-data">
     <label for="text">Enter text (maximum 3000 characters):</label>
     <textarea id="text" name="text" rows="4" cols="50" maxlength="3000"></textarea>
 
     <label for="post_image">Select JPG or PNG file:</label>
     <input type="file" id="post_image" name="post_image" accept=".jpg, .jpeg, .png">
-    <input type="submit" name="register" value="Register">
+    <input type="submit" name="register" value="Submit">
     </form>
 
     
