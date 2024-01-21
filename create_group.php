5a116939e10c5774e49c2a83d0e45fde8489bb4f
Ethan Smyth <es2005@hw.ac.uk>
2024-01-21 16:39:23 +0000

    update create_group

--- create_group.php
+++ create_group.php
@@ -1,7 +1,12 @@
 <?php
+require_once "connect_db";
 if (isset($_POST['listValues'])) {
-    $inputValues = $_POST['listValues'];
-    $valuesArray = explode(', ', $inputValues);
+    $inputUsers = $_POST['listValues'];
+    $usersArray = explode(', ', $inputUsers); // Seperate the user's inputted comma seperated list of users and break them apart into an array
+    foreach($usersArray as $user){
+    pg_query("SELECT * FROM users WHERE username =" . "'" . $user . "';");
+    }
+
 }
 ?>
 
@@ -16,7 +21,7 @@ if (isset($_POST['listValues'])) {
 <form action="create_group.php" method="post">
 <label for="group_name">Group name:</label> 
   <input id="group_name" name="name" type="text"/>
-  <label for="listValues">Enter a list of initial group member usernames (not including yourself) separated by commas:</label>
+  <label for="listValues">Enter a list of initial group member usernames (not including yourself) separated by commas with a space after each comma:</label>
   <input type="text" id="listValues" name="listValues">
   <br>
   <input type="submit" value="Submit">
