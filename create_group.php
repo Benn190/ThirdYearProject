73aeec642458434b3f7c3b1a88127a0e892681b5
Ethan Smyth <es2005@hw.ac.uk>
2024-01-22 08:49:03 +0000

    add comments

--- create_group.php
+++ create_group.php
@@ -9,21 +9,21 @@ if (isset($_POST['listValues']) && isset($_POST["name"])) {
     $groupName = $_POST['name'];
     $usersArray = explode(', ', $inputUsers); // Seperate the user's inputted comma seperated list of users and break them apart into an array
 
-    $createGroupSTMT = pg_prepare($conn, "createGroup", "INSERT INTO groups (group_name, managerID) VALUES ($1, $2) RETURNING groupID");
+    $createGroupSTMT = pg_prepare($conn, "createGroup", "INSERT INTO groups (group_name, managerID) VALUES ($1, $2) RETURNING groupID"); // add the group to the database
     $createGroupRESULT = pg_execute($conn, "createGroup", array($groupName, $_SESSION["user_id"]));
-    $groupID = pg_fetch_assoc($createGroupRESULT)['groupid'];
+    $groupID = pg_fetch_assoc($createGroupRESULT)['groupid']; // Get the group id of the newly created group to be used when adding members to the group
 
     $usernameQuerySTMT = pg_prepare($conn, "usernameQuery", "SELECT * FROM users WHERE username = $1");
     $addGroupMemberSTMT = pg_prepare($conn, "addGroupMember", "INSERT INTO groupMembers (groupID, userID) VALUES ($1, $2)");
 
     foreach($usersArray as $user){
       $usernameQueryRESULT = pg_execute($conn, "usernameQuery", array($user));
-      $numRows = pg_num_rows($usernameQueryRESULT);
+      $numRows = pg_num_rows($usernameQueryRESULT); // Check if there are any users with that username
       if ($numRows == 0){
         echo ("No users with that name!");
       } else{
-        $userID = pg_fetch_assoc($usernameQueryRESULT)["user_id"];
-        $addGroupMemberRESULT = pg_execute($conn, "addGroupMember", array($groupID, $userID));
+        $userID = pg_fetch_assoc($usernameQueryRESULT)["user_id"]; // Grab the user_id of the user with the queried username if they do exist
+        $addGroupMemberRESULT = pg_execute($conn, "addGroupMember", array($groupID, $userID)); // Add them to the database as a member of the created group
       }
     }
 
