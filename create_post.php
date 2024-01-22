3edd971d47d0c060c0566a38702a3541dc2f9eda
Ethan Smyth <es2005@hw.ac.uk>
2024-01-22 08:51:37 +0000

    add comments

--- create_post.php
+++ create_post.php
@@ -1,16 +1,14 @@
 <?php
     session_start();
     if (! isset($_SESSION["user_id"])){
-        header('Location: '."./register.php");
+        header('Location: '."./register.php"); // redirect the user to the register page if they have not already logged in
     }
     if (isset($_POST['register'])) {
         // Connect to the database
         require_once "connect_db.php";
     
-        // Prepare and execute the SQL statement
         $stmt = pg_prepare($conn, "insert_post", "INSERT INTO posts (user_id, text) VALUES ($1, $2) RETURNING post_id");
         
-        // Get the form data
         $user_id = $_SESSION['user_id'];
         $text = $_POST['text'];
 
@@ -25,14 +23,13 @@
     
         $uploaddir = './post_images/';
         $uploadfile = $uploaddir . "post_image". $post_id . ".png";
-
+        //Add the image associated with the post to the post_images directory with the the number of the post_id linked to the image so it can be found easily
         if(move_uploaded_file($_FILES['post_image']['tmp_name'], $uploadfile)){
         echo "File was successfully uploaded.\n";
-        /* Your file is uploaded into your server and you can do what ever you want with */
         }else{
         echo "Possible file upload attack!\n";
         }
-    
+        
         // Close the connection
         pg_close($conn);
     }
