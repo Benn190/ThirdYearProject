35c46709ef78c1ea049be1ebaf953b47d489c555
Ethan Smyth <es2005@hw.ac.uk>
2024-01-23 10:22:51 +0000

    update post_comment

--- post_comment.php
+++ post_comment.php
@@ -1,5 +1,19 @@
 <?php
+require_once "connect_db.php";
+session_start();
+if (! isset($_SESSION["user_id"])){
+    header('Location: '."./register.php");
+  }
+if (isset($_POST['comment']) && isset($_POST['post_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $post_id = $_POST['post_id'];
+    $comment = $_POST['comment'];
 
+    //Add the comment to the database
+    $add_commentsSTMT = pg_prepare($conn, "add_comment", "INSERT INTO comments (user_id, post_id, text) VALUES ($1, $2, $3)");
+    $add_commentRESULT = pg_execute($conn, "add_comment", array($user_id, $post_id, $comment));
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +25,11 @@
 </head>
 <body>
   <form action="post_comment.php" method="post"></form>
+<?php
+$post_id = 10;
+echo("<input type='hidden' id='custId' name='custId' value=$post_id>");
+?>
     <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
-    <button type="submit"></button>
+    <button name="submit" type="submit"></button>
 </body>
 </html>
