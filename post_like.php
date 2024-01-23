3ffe85d1dd6697e8cdcc22e3cae8080a99cbb9c1
Ethan Smyth <es2005@hw.ac.uk>
2024-01-23 09:59:16 +0000

    add remove like functionality

--- post_like.php
+++ post_like.php
@@ -5,14 +5,21 @@ if (! isset($_SESSION["user_id"])){
     header('Location: '."./register.php");
   }
 if (isset($_POST['like']) && isset($_POST['post_id'])) {
-    $check_likeSTMT = pg_prepare($conn, "check_like", "SELECT * FROM likes WHERE user_id = $1 AND post_id = $2");
-    $check_likeRESULT = pg_execute($conn, "check_like", array($user_id, $post_id));
-    
-    $add_likeSTMT = pg_prepare($conn, "add_like", "INSERT INTO likes (user_id, post_id) VALUES ($1, $2)");
-
     $user_id = $_SESSION['user_id'];
     $post_id = $_POST['post_id'];
-    $add_likeRESULT = pg_execute($conn, "add_like", array($user_id, $post_id));
+    $check_likeSTMT = pg_prepare($conn, "check_like", "SELECT * FROM likes WHERE user_id = $1 AND post_id = $2");
+    $check_likeRESULT = pg_execute($conn, "check_like", array($user_id, $post_id));
+    $alreadyLiked = pg_num_rows($check_likeRESULT) > 0;
+
+    if ( ! $alreadyLiked) {
+        $add_likeSTMT = pg_prepare($conn, "add_like", "INSERT INTO likes (user_id, post_id) VALUES ($1, $2)");
+        $add_likeRESULT = pg_execute($conn, "add_like", array($user_id, $post_id));
+        echo "liking!";
+    } else{
+        $remove_likeSTMT = pg_prepare($conn, "remove_like", "DELETE FROM likes WHERE user_id = $1 AND post_id = $2");
+        $remove_likeRESULT = pg_execute($conn, "remove_like", array($user_id, $post_id));
+        echo "removing like";
+    }
 }
 ?>
 
