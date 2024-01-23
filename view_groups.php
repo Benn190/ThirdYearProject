1c35d8f7b78378fdf03aba572d0eacdc53d669f8
jku2000@hw.ac.uk <jku2000@hw.ac.uk>
2024-01-23 09:17:02 +0000

    display groups

---
+++ view_groups.php
@@ -0,0 +1,29 @@
+<?php
+#opening session
+session_start();
+#connect to database
+require_once "connect_db.php";
+#if user not log in - display registe.php page
+if (! isset($_SESSION["user_id"])){
+  header('Location: '."./register.php");
+}
+#display groups which contanin user's id 
+$q = "SELECT * FROM groups WHERE id={$_SESSION[user_id]}
+ORDER BY post_date DESC";
+$r = pg_query($conn, $q);
+
+if (pg_num_rows($r) > 0) {
+    while ($row = pg_fetch_assoc($r)) 
+    {
+        echo '<div class="col-sm-12 col-md-4">
+        <p>'.$row['group_name'].'</p>
+        
+        
+    </div>';
+    }
+}
+
+
+pg_close($conn);
+
+?>
