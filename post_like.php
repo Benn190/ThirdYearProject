39f3e3213e0c538ca3387a08bae8118c9db4d172
Ethan Smyth <es2005@hw.ac.uk>
2024-01-23 09:42:42 +0000

    update post_like

--- post_like.php
+++ post_like.php
@@ -1,3 +1,15 @@
+<?php
+require_once "connect_db.php";
+session_start();
+if (isset($_POST['like']) && isset($_POST['post_id'])) {
+    $add_likeSTMT = pg_prepare($conn, "insert_user", "INSERT INTO likes (user_id, post_id) VALUES ($1, $2)");
+
+    $user_id = $_SESSION['user_id'];
+    $post_id = $_POST['post_id'];
+    $add_likeRESULT = pg_execute($conn, array($user_id, $post_id));
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +18,13 @@
     <title>Document</title>
 </head>
 <body>
-    <form action=""></form>
+<form method='post' action='post_like.php'>
+<?php
+$post_id = 10;
+echo "<input type='hidden' name='post_id' value='$postId'>"
+?>
+<input type='submit' name='like' value='Like'>
+</form>
     
 </body>
 </html>
