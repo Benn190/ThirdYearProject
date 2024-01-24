d3e0d1dffbf3d3bee3df40e68154bb80c2a91092
Ethan Smyth <es2005@hw.ac.uk>
2024-01-24 14:36:32 +0000

    update redirect

--- delete_post.php
+++ delete_post.php
@@ -1,3 +1,18 @@
+<?php
+require_once "connect_db.php";
+session_start();
+if (! isset($_SESSION["user_id"])){
+    header('Location: '."./register.php");
+  }
+if (isset($_POST['like']) && isset($_POST['post_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $post_id = $_POST['post_id'];
+    
+        $remove_likeSTMT = pg_prepare($conn, "remove_like", "DELETE FROM posts WHERE user_id = $1 AND post_id = $2");
+        $remove_likeRESULT = pg_execute($conn, "remove_like", array($user_id, $post_id));
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +21,14 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="delete_post.php" method="post"></form>
-    <input type="button" value="delete_post">
+<form method='post' action='delete_post.php'>
+<?php
+//hardcoded for testing
+$post_id = 10;
+echo "<input type='hidden' name='post_id' value='$post_id'>"
+?>
+<input type='submit' name='delete' value='delete'>
+</form>
+    
 </body>
 </html>
