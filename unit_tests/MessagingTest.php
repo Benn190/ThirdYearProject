692c77df6e1b87909160d59c634f565ab40c26e2
imant686 <119474127+imant686@users.noreply.github.com>
2024-01-30 13:20:11 +0000

    Create MessagingTest.php

---
+++ unit_tests/MessagingTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+use PHPUnit\Framework\TestCase;
+class ChatSystem {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function testSendingMessages($text){
+
+        $result = $this->db->execute($text);
+        if (count($result) == 0) {
+            return 'Please insert a message';
+        }
+
+}
+}
