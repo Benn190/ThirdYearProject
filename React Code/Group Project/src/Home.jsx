697f4ca67a49462a4a313a19077323c71b361ebd
kg2020 <kg2020@hw.ac.uk>
2024-02-13 14:02:40 +0000

    react code

---
+++ React Code/Group Project/src/Home.jsx
@@ -0,0 +1,13 @@
+import HomeMain from "./components/Home/home-main";
+import NavBar from "./components/navbar/navbar";
+
+function Home() {
+    return (
+        <>
+            <NavBar />
+            <HomeMain />
+        </>
+    );
+}
+
+export default Home;
