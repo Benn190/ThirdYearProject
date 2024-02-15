c3c0960b589cb461d1f21aaa6bca5c446ec02654
Ben Harris <bh2013@hw.ac.uk>
2024-02-15 11:27:59 +0000

    update

---
+++ GroupProject/src/Pages/Group/Group.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/navbar/navbar";
+import GroupMain from "./GroupMain";
+import "./css/Group.css";
+
+function Group() {
+    return (
+        <>
+            <NavBar />
+            <GroupMain/>
+        </>
+    );
+}
+export default Group;
---
+++ GroupProject/src/Pages/Group/GroupMain.jsx
@@ -0,0 +1,15 @@
+
+import Friends from "./Friends";
+import GroupContainer from "./GroupContainer";
+
+function GroupMain() {
+    return (
+        <main>
+        <GroupContainer/>
+        <Friends />
+        </main>
+        
+    );
+}
+export default GroupMain;
+
