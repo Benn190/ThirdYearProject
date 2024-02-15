b50fd37910c9d0e31138f578c8cad88c61ed7e1b
kg2020 <kg2020@hw.ac.uk>
2024-02-15 20:11:03 +0000

    trying to route

--- GroupProject/src/App.jsx
+++ GroupProject/src/App.jsx
@@ -1,13 +1,22 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
 import "./App.css";
 import Group from "./Pages/Group/Group";
 import Home from "./Pages/Home/home";
+import Login from "./Pages/Login/login";
 import Profile from "./Pages/Profile/profile";
 
 function App() {
     return (
-        <>
-            <Home></Home>
-        </>
+        <Router>
+            <Switch>
+                <Route path="./Pages/Home/home" exact component={Home} />
+                <Route path="./Pages/Group/Group" component={Group} />
+                <Route path="./Pages/Login/login" component={Login} />
+                <Route path="./Pages/Profile/profile" component={Profile} />
+                <Route component={Home} />
+            </Switch>
+        </Router>
     );
 }
 
