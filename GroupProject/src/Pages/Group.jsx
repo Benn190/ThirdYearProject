d8aae18661254e77a6979c87c4bc1d85f5ab9f27
kg2020 <kg2020@hw.ac.uk>
2024-02-16 13:07:09 +0000

    react

---
+++ GroupProject/src/Pages/Group.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/navbar";
+import GroupMain from "./Group/GroupMain";
+
+function Group() {
+    return (
+        <>
+            <NavBar />
+            <GroupMain />
+        </>
+    );
+}
+export default Group;
---
+++ GroupProject/src/Pages/Group/GroupMain.jsx
@@ -0,0 +1,17 @@
+import Friends from "./Friends";
+import GroupContainer from "./GroupContainer";
+import "./css/Group.css";
+
+function GroupMain() {
+<<<<<<< HEAD
+    return <main className="grouppage">Group</main>;
+=======
+    return (
+        <>
+            <GroupContainer />
+            <Friends />
+        </>
+    )
+>>>>>>> 9c242cb8c5d63acd83be1556b5170e8d0ef9ef6a
+}
+export default GroupMain;
