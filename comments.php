fdf3912e0fa6a6108b9b41a85f79f9486f8f05e3
imant686 <119474127+imant686@users.noreply.github.com>
2024-02-23 19:16:34 +0000

    Update comments.php
    edit button still in process

--- comments.php
+++ comments.php
@@ -18,7 +18,28 @@ function setComments($conn){
         $result = pg_query_params($conn, $query, array($user_id, $date, $message));
 
     }
-    
-
 }
+    function getComments($conn){
+
+        $query = "SELECT * FROM comments";
+        $result = pg_query($conn, $query);
+        while($row = pg_fetch_assoc($result)){
+            echo "<div class  = 'comments-box'><p>";
+                echo $row['user_id']."<br><br>";
+                echo $row['date']."<br><br>";
+                echo nl2br($row['message']);
+            echo  "</p>
+                <form class = 'edit-form' method = 'POST' action = 'editcomment.php'>
+                    <input type='hidden' name='id' value ='" .$row['id']."' >
+                    <input type='hidden' name='user_id' value ='" .$row['user_id']."' >
+                    <input type='hidden' name='user_id' value ='" .$row['user_id']."' >
+                    <input type='hidden' name='date' value ='" .$row['date']."' >
+                    <input type='hidden' name='message' value ='" .$row['message']."' >
+                    <button>Edit</button>
+                </form>
+            </div>";
+
+
+        }
+    }
 ?>
