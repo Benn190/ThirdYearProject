738d68772cea1491c7671758c141c36963323234
imant686 <119474127+imant686@users.noreply.github.com>
2024-02-24 16:46:41 +0000

    Update index.php
    added edit and delete comment feature

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ date_default_timezone_set("Europe/London");
 echo" <form method = 'POST' action = '".setComments($conn)."' >
     <input type='hidden' name='user_id' value='Anonymous'>
     <input type='hidden' name='date' value='". date('Y-m-d H:i:s')."'>
-    <textarea name='message'></textarea> <br>
+    <textarea name='message'></textarea><br>
     <button type='submit' name = 'commentSubmit'>Comment</button>
 </form>";
 
