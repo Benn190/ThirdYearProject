052bf192ebcf844a2ac72c0e2e04d82440c60f31
justyna-ulejczyk <146101244+justyna-ulejczyk@users.noreply.github.com>
2024-03-12 17:21:54 +0000

    Update login.php

--- php/login.php
+++ php/login.php
@@ -2,12 +2,15 @@
 // Connect to the database
 require_once "connect_db.php";
 
-// Prepare the SQL statement with a placeholder for the username
-$stmt = pg_prepare($conn, "find_user", "SELECT * FROM accounts WHERE username = $1");
+// Start the session
+//session_start();
 
 // Get username and password from POST data
 $username = $_POST['username'];
 $user_entered_password = $_POST['password'];
+
+// Prepare the SQL statement with a placeholder for the username
+$stmt = pg_prepare($conn, "find_user", "SELECT * FROM accounts WHERE username = $1");
 
 // Execute the prepared statement with the provided username
 $result = pg_execute($conn, "find_user", array($username));
@@ -25,9 +28,10 @@ $stored_password_hash = pg_fetch_result($result, 0, 'password');
 if (!password_verify($user_entered_password, $stored_password_hash)) {
     echo "Incorrect password";
 } else {
-    // Start a session and store the username
-    session_id("userSession");
-    session_start();
+    // Regenerate session ID to prevent session fixation
+    session_regenerate_id(true);
+
+    // Store the username in the session
     $_SESSION['username'] = $username;
 
     // Redirect to home.php
