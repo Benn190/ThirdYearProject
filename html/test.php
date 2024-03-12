783f8768752b36fabf5d29f95faee6f73b01972d
jku2000@hw.ac.uk <jku2000@hw.ac.uk>
2024-03-12 12:15:04 +0000

    changes

--- html/test.php
+++ html/test.php
@@ -8,14 +8,20 @@ $password = "cFs0XFyubfNelPRIoab5EJcJX6XGA1Y5";
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if ($conn) {
-    $query = pg_prepare($conn, "a", "SELECT * FROM accounts ");
-    $result = pg_execute($conn, "a");
-    while($row = pg_fetch_assoc($result))
-    {
-        echo $row['username'];
-
+    $query = pg_prepare($conn, "a", "SELECT * FROM accounts");
+    if (!$query) {
+        echo "Error preparing query: " . pg_last_error($conn);
+    } else {
+        $result = pg_execute($conn, "a");
+        if (!$result) {
+            echo "Error executing query: " . pg_last_error($conn);
+        } else {
+            while($row = pg_fetch_assoc($result)) {
+                echo $row['username'];
+            }
+            echo "Data fetched successfully!";
+        }
     }
-    echo "Connection to database successful!";
 } else {
     echo "Cannot connect to database!";
 }
