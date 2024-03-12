fb19dc9e5f1cb32374b8aeb6a154f9c6de88abec
jku2000@hw.ac.uk <jku2000@hw.ac.uk>
2024-03-12 12:07:30 +0000

    tests

--- html/test.php
+++ html/test.php
@@ -8,6 +8,13 @@ $password = "cFs0XFyubfNelPRIoab5EJcJX6XGA1Y5";
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if ($conn) {
+    $query = pg_prepare($conn, "a", "SELECT * FROM accounts ");
+    $result = pg_execute($conn, "a");
+    while($row = pg_fetch_assoc($result))
+    {
+        echo $row['username'];
+
+    }
     echo "Connection to database successful!";
 } else {
     echo "Cannot connect to database!";
