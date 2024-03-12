cab70451de84bfe982672e5f55e776a4137aa1e3
justyna-ulejczyk <146101244+justyna-ulejczyk@users.noreply.github.com>
2024-03-12 11:36:34 +0000

    Update connect_db.php

--- php/connect_db.php
+++ php/connect_db.php
@@ -1,13 +1,6 @@
-<?php # CONNECT TO PostgreSQL DATABASE.
-$host = "kandula.db.elephantsql.com";
-$port = "5432";
-$dbname = "vfqjpwel";
-$user = "vfqjpwel";
-$password = "cFs0XFyubfNelPRIoab5EJcJX6XGA1Y5";
-
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+<?php
 if (!$conn) { 
 # Display error masseage, if fail 
-die('Could not connect to MySQL: ' . pg_last_error()); 
+die('Could not connect to pgSQL: ' . pg_last_error()); 
 } 
 
